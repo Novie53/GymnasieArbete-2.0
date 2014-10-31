0bc919f74d7c72d9c2abefe5fb2cecf70469ef8c
Novie53 <martenolsson95@gmail.com>
2014-10-31 12:09:46 +0100

    Skapade Icon i system Tray

--- GymnasieArbete/Logging Program/Form1.cs
+++ GymnasieArbete/Logging Program/Form1.cs
@@ -12,9 +12,36 @@ namespace Logging_Program
 {
     public partial class Form1 : Form
     {
+        private NotifyIcon notifyIcon1 = new NotifyIcon();
+
         public Form1()
         {
             InitializeComponent();
+            notifyIcon1.Icon = Icon.ExtractAssociatedIcon(@"C:\Program Files (x86)\Mozilla Firefox\firefox.exe");
+            notifyIcon1.DoubleClick += notifyIcon1_DoubleClick;
+            
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "Minimize to Tray App";
+            notifyIcon1.BalloonTipText = "You have successfully minimized your form.";
+
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(5000);
+                this.Hide();
+            }
+            else if (this.WindowState == FormWindowState.Normal)
+            {
+                notifyIcon1.Visible = false;
+            }
         }
     }
 }
