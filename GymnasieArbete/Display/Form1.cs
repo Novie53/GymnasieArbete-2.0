6c532f6c37993e11fcde4a470cf047a1085c541f
Novie53 <martenolsson95@gmail.com>
2014-12-11 14:16:41 +0100

    Början till Display

--- GymnasieArbete/Display/Form1.cs
+++ GymnasieArbete/Display/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Logging_Program;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,17 @@ namespace Display
         public Form1()
         {
             InitializeComponent();
+            string dbConString = @"Data Source=" + Config.databasePATH + @";Version=3;";
+            using (DatabaseConncter dbConnector = new DatabaseConncter(dbConString))
+            {
+                //Console.WriteLine(dbConnector.getTableCount("dota_matches"));
+                dataGridView1.DataSource = dbConnector.ExecuteQuery(@"SELECT * FROM dota_matches");
+            }
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            dataGridView1.Width = this.Width-80;
+            dataGridView1.Height = this.Height-100;
         }
     }
 }
